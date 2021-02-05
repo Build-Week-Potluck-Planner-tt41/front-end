a9008bc561fab30eb56cb6b1ef580ba47f24c58d
brandon santos <brandonx1123@gmail.com>
2021-02-05 04:29:53 -0500

    fixed whatever was broken

--- potluck/src/components/addGuestFood.js
+++ potluck/src/components/addGuestFood.js
@@ -1,54 +1,63 @@
-import React ,{useState} from 'react'
-import {useParams, useHistory} from 'react-router-dom'
-import axios from 'axios'
-import {axiosWithAuth} from '../utils/axioswithauth'
+import React, { useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+import { axiosWithAuth } from "../utils/axioswithauth";
 
-const initialPotLuck ={
-    food:'',
-    // food:''
-}
+const initialPotLuck = {
+  food: "",
+  // food:''
+};
 
-const AddPotLuck = (props) =>{
+const AddPotLuck = (props) => {
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [food, setFood] = useState(initialPotLuck);
 
-    const {push} = useHistory()
-    const {id} =useParams()
-    const [food, setFood] =useState(initialPotLuck)
+  const handleChange = (e) => {
+    setFood({
+      ...food,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleChange = e =>{
-        setFood({
-            ...food,
-            [e.target.name]:e.target.value
-        })
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //need an endpoint as a guest to post the food i want to bring and update API( please without 2 ids, its CRAZY)
+    axios
+      .post("", food)
+      .then((res) => {
+        console.log("res when posting:", res);
+        setFood(initialPotLuck);
+        push("/display");
+      })
+      .catch((err) => {
+        console.log("error posting Potluck:", err);
+      });
+  };
 
-    }
+  return (
+    <div className="added-potluck">
+      <form onSubmit={handleSubmit}>
+        <label>Desired Food To Bring:</label>
+        <input
+          name="potLuckName"
+          onChange={handleChange}
+          type="text"
+          value={food.food}
+          placeholder="Enter Food You Want To Bring"
+        ></input>
 
-    const handleSubmit =(e) => {
-        e.preventDefault()
-        //need an endpoint as a guest to post the food i want to bring and update API( please without 2 ids, its CRAZY)
-        axios.post('', food) 
-        .then((res) =>{
-            console.log('res when posting:',res)
-            setFood(initialPotLuck)
-            push('/display')
-        })
-        .catch((err)=>{
-            console.log('error posting Potluck:', err)
-        })
-    }
-
-    return(
-        <div className='added-potluck'>
-            <form onSubmit={handleSubmit}>
-            <label>Potluck Name:</label>
-            <input name='potLuckName' onChange={handleChange} type='text' value={food.food} placeholder='Enter Potluck Name'></input>
-           
-                <button>Add Your Food!</button>
-                <button onClick={()=>{
-                    push('/display')
-                }}>Show Potlucks Already In</button>
-            </form>
-        </div>
-    )
-}
+        <button>Add Your Food!</button>
+        <button
+          onClick={() => {
+            push("/display");
+          }}
+        >
+          Show Potlucks Already In
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default AddPotLuck;
